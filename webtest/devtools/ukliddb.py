4ca220064697f87ffb5130dd5cc654eec3004606
mz10 <mz10@users.noreply.github.com>
2018-04-11 18:34:36 +0200

    skript pro vymazani DB

---
+++ webtest/devtools/ukliddb.py
@@ -0,0 +1,27 @@
+# vymaze vsechny tabulky z databaze
+
+import psycopg2
+
+udaje = {
+    "host": "localhost",
+    "user": "postgres",
+    "database": "webtest",
+    "password": "a"
+}
+
+db = psycopg2.connect(**udaje)
+sql = db.cursor()
+db.autocommit = True
+
+info = "Vsechny tyto tabulky byly smazany:\n"
+
+#smazat tabulku:
+sql.execute("select * from pg_tables where schemaname = 'public';");   
+for radek in sql.fetchall():
+    info += radek[1] + "\n"
+    sql.execute('DROP TABLE "' + radek[1] + '" CASCADE;')
+
+print(info)
+
+sql.close()
+db.close()
